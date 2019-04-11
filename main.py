45444cf3550303f2b4459f711f74b61b8a9b6804
llyptical <ggalvan1998@gmail.com>
2019-04-11 11:41:04 -0700

    Update Student.csv and main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import csv
+
+with open('Students.csv', mode='r') as students:
+    r = csv.DictReader(students)
+    n = 0
+    for row in r:
+        if n == 0:
+            print(f'Column names are {", ".join(row)}')
+            n += 1
+        print(f'{row["first"]}\t\t{row["last"]}\t\t{row["email"]}')
+        n += 1
+    print(f'Processed {n} lines.')
